1c02971efe255b1b4c4881c52d0d80e2268e4209
Vallesuski <83090643+Vallesuski@users.noreply.github.com>
2025-01-07 11:02:41 -0500

    Update app.php

--- src/Configs/app.php
+++ src/Configs/app.php
@@ -5,7 +5,7 @@ use App\Installer\Database\MySqlSetup;
 return [
     'min_php_version' => '8.2',
 
-    'repository' => 'https://github.com/f1carreras/UNIT3D-Community-Edition-v8.3.4.git',
+    'repository' => 'https://github.com/Vallesuski/UNIT3D-Community-Edition-v8.3.4.git',
 
     'database_installers' => [
         /**
